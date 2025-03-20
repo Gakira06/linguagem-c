49fa290e73035d3c94f757c3cdc7cf209aa592f1
Gakira06 <gakirakiyohara@gmail.com>
2025-03-20 13:43:57 -0300

    Delete OneDrive/FIAP/programas/C/forca.c

--- OneDrive/FIAP/programas/C/forca.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	char palavrasecreta[20];
-
-    palavrasecreta[0] = 'M';
-    palavrasecreta[1] = 'E';
-
-    printf("%c%c", palavrasecreta[0], palavrasecreta[1]);
-}
